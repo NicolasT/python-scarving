56f0ed44c5942b79da83c79a9337121af2f27e28
Nicolas Trangez <ikke@nicolast.be>
2007-08-31 23:05:24 +0200

    Class-based rewite
    This version uses PIL's Kernel filter to perform the Sobel transformation. The result isn't equal to the previous Python implementation somehow, which causes bad paths to be calculated.

--- scarve.py
+++ scarve.py
@@ -1,4 +1,4 @@
-# scrave.py - Seam Craving based image resizing algorithm
+# scarve.py - Seam Craving based image resizing algorithm
 #
 # This implementation is highly inefficient, can only resize width
 # and most likely contains several bugs and algorithmic nonsense.
@@ -21,181 +21,177 @@
 #
 # EOL
 
-from PIL import Image
+from PIL import Image, ImageFilter, ImageChops, ImageOps
 import sys
+import math
 
-def create_edges(orig):
-        ret = Image.new(orig.mode, orig.size)
-        op = orig.load()
-        rp = ret.load()
+class SeamCarve:
+        def __init__(self, image):
+                self._original = image
+                self._carves = image.copy()
+                self._energy = None
+                self._resized = None
+                self._costs = None
+                self._maxcost = 1
 
-        sx = ( (-1, 0, 1),
-               (-2, 0, 2),
-               (-1, 0, 1) )
-        sy = ( (1, 2, 1),
-               (0, 0, 0),
-               (-1, -2, -1) )
+        def get_energy_image(self):
+                if self._energy == None:
+                        self._calculate_energy()
+                return self._energy
 
-        oox = -2
-        ooy = -2
-        os = 1.0 / 3.0
-
-        for y in range(0, orig.size[1]):
-                for x in range(0, orig.size[0]):
-                        tx = 0
-                        ty = 0
-
-                        for oy in range(0, len(sy)):
-                                for ox in range(0, len(sx)):
-                                        hx = x + ox + oox
-                                        hy = y + oy + ooy
-                                        if hx >= 0 and hx < orig.size[0] and hy >= 0 and hy < orig.size[1]:
-                                                (r, g, b) = op[hx, hy]
-                                                tx = tx + ((r + g + b) * sx[oy][ox] * os)
-                                                ty = ty + ((r + g + b) * sy[oy][ox] * os)
-                        total = abs(tx) + abs(ty)
-                        if total < 0:
-                                total = 0
-                        if total > 255:
-                                total = 255
-
-                        total = int(total)
-                        rp[x, y] = (total, total, total)
-
-        return ret
-
-def get_weight(image, x, y):
-        return image.getpixel((x, y))[0]
-
-def find_path(edges, show = False):
-        w = edges.size[0]
-        h = edges.size[1]
-
-        inf = float("infinity")
-
-        costs = []
-        for y in range(0, h):
-                costs.append([inf for i in range(0, w)])
-
-        for x in range(0, w):
-                costs[0][x] = 0.0
-
-        maxcost = 0.0
-        for y in range(1, h):
-                for x in range(0, w):
-                        bestx = x
-                        bestcost = costs[y - 1][x]
-                        for tx in range(x - 1, x + 2):
-                                if tx >= 0 and tx < w:
-                                        if costs[y - 1][tx] < bestcost:
-                                                bestx = tx
-                                                bestcost = costs[y - 1][tx]
-                        costs[y][x] = costs[y - 1][bestx] + get_weight(edges, x, y)
-
-                        if costs[y][x] > maxcost:
-                                maxcost = costs[y][x]
-
-        if show == True:
-                ret = Image.new(edges.mode, edges.size)
+        def get_costs_image(self):
+                if self._costs == None:
+                        self._calculate_costs()
+                ret = Image.new(self._original.mode, self._original.size)
+                (w, h) = ret.size
                 rp = ret.load()
                 for y in range(0, h):
                         for x in range(0, w):
-                                v = int((costs[y][x] * 255) / maxcost)
+                                v = int((self._costs[y][x] * 255) / self._maxcost)
                                 rp[x, y] = (v, v, v)
-                ret.show()
+                return ret
 
 
-        if show == True:
-                print "Building path"
+        def resize_width(self, pixels):
+                if self._energy == None:
+                        self._energy = self._calculate_energy()
 
-        path = []
-        at = -1
-        mincost = inf
-        for x in range(0, w):
-                if costs[h - 1][x] < mincost:
-                        at = x
-                        mincost = costs[h - 1][x]
+        def _calculate_energy(self):
+                #Sobel matrix coefficients. See http://en.wikipedia.org/wiki/Sobel
+                sx = ( -1, 0, 1,
+                       -2, 0, 2,
+                       -1, 0, 1 )
+                sy = ( 1, 2, 1,
+                       0, 0, 0,
+                       -1, -2, -1 )
 
-        path.append(at)
+                grayscale = ImageOps.grayscale(self._original)
+                x = grayscale.filter(ImageFilter.Kernel((3, 3), sx, scale = 1.0))
+                y = grayscale.filter(ImageFilter.Kernel((3, 3), sy, scale = 1))
+                self._energy = ImageChops.add(x, y)
 
-        for y in range(h - 2, -1, -1):
-                next = at
-                nc = costs[y][at]
+        def _calculate_costs(self):
+                (w, h) = self._original.size
+                inf = float("infinity")
+                if self._energy == None:
+                        self._calculate_energy()
+                energy_data = self._energy.load()
 
-                for lx in range(at - 1, at + 2):
-                        if lx >= 0 and lx <= w:
-                                if costs[y][lx] < nc:
-                                        nc = costs[y][lx]
-                                        next = lx
-                at = next
+                costs = []
+                line = [inf for i in range(0, w)]
+                #Fill costs array, everything got infinite cost
+                for y in range(0, h):
+                        costs.append(list(line))
+
+                #Cost of items on first row is 0
+                for x in range(0, w):
+                        costs[0][x] = 0.0
+
+                maxcost = 0.0
+                for y in range(1, h):
+                        for x in range(0, w):
+                                bestx = x
+                                bestcost = costs[y - 1][x]
+                                for tx in range(x - 1, x + 2):
+                                        if tx >= 0 and tx < w:
+                                                if costs[y - 1][tx] < bestcost:
+                                                        bestx = tx
+                                                        bestcost = costs[y - 1][tx]
+                                costs[y][x] = costs[y - 1][bestx] + energy_data[x, y]
+
+                                if costs[y][x] > maxcost:
+                                        maxcost = costs[y][x]
+
+                self._costs = costs
+                self._maxcost = maxcost
+
+        def _find_path(self):
+                if self._costs == None:
+                        self._calculate_costs()
+                path = []
+                at = -1
+                mincost = float("infinity")
+                (w, h) = self._original.size
+
+                for x in range(0, w):
+                        if self._costs[h - 1][x] < mincost:
+                                at = x
+                                mincost = self._costs[h - 1][x]
+
                 path.append(at)
 
-        path.reverse()
+                for y in range(h - 2, -1, -1):
+                        next = at
+                        next_cost = self._costs[y][at]
 
-        if show == True:
-                t = edges.copy()
-                tp = t.load()
+                        for cx in range(at - 1, at + 2):
+                                if cx >= 0 and cx <= w:
+                                        if self._costs[y][cx] < next_cost:
+                                                next_cost = self._costs[y][cx]
+                                                next = cx
+
+                        at = next
+                        path.append(at)
+
+                path.reverse()
+                return path
+
+        def get_path_image(self):
+                if self._energy == None:
+                        self._calculate_energy()
+                ret = self._energy.convert("RGB")
+                rp = ret.load()
+                path = self._find_path()
+                for y in range(0, ret.size[1]):
+                        rp[path[y], y] = (255, 0, 0)
+                return ret
+
+        def _carve(self):
+                path = self._find_path()
+                (w, h) = self._original.size
+
+                nw = w - 1
+                nh = h
+
+                ret = Image.new(self._original.mode, (nw, nh))
+
+                op = self._original.load()
+                rp = ret.load()
+
                 for y in range(0, h):
-                        tp[path[y], y] = (0, 255, 0)
-                t.show()
+                        for x in range(0, w):
+                                nx = x
+                                if x >= path[y]:
+                                        nx = nx - 1
+                                if x == path[y]:
+                                        cl = op[x, y]
+                                        cr = op[x + 1, y]
 
-        return path
+                                        rp[nx, y] = tuple([(cl[i] + cr[i]) / 2 for i in range(0, len(cl))])
+                                else:
+                                        rp[nx, y] = op[x, y]
 
-def crave(image, path):
-        ow = image.size[0]
-        oh = image.size[1]
+                self._resized = ret
 
-        w = ow - 1
-        h = oh
-
-        ret = Image.new(image.mode, (w, h))
-
-        op = image.load()
-        rp = ret.load()
-
-        for y in range(0, oh):
-                for x in range(0, ow):
-                        nx = x
-                        if x >= path[y]:
-                                nx = nx - 1
-
-                        if x == path[y]:
-                                cl = op[x, y]
-                                cr = op[x + 1, y]
-
-                                nc = ((cl[0] + cr[0]) / 2, (cl[1] + cr[1]) / 2, (cl[2] + cr[2]) / 2)
-                                rp[nx, y] = nc
-                        else:
-                                rp[nx, y] = op[x, y]
-
-        return ret
+        def get_carved(self):
+                self._carve()
+                return self._resized
 
 
-def runit(image):
-        print "Edge detection"
-        edges = create_edges(image)
-        #edges.show()
-        print "Calculating costs"
-        path = find_path(edges, False) #True)
-        print "Craving"
-        craved = crave(image, path)
-        #craved.show()
-        return craved
 
 def main():
         times = int(sys.argv[1])
         print "Loading original image"
         image = Image.open(sys.argv[2])
-        image.show()
 
-        craved = image
+        carved = image
         for i in range(0, times):
                 print "Run %d" % i
-                craved = runit(craved)
-                #craved.show()
+                c = SeamCarve(carved)
+                carved = c.get_carved()
 
-        craved.show()
-        craved.save("craved.jpg")
+        carved.show()
+        carved.save("carved.jpg")
 
 if __name__ == "__main__":
         main()

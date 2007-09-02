74f26fd3c13154c12759c2dbc589f059c813c883
Nicolas Trangez <ikke@nicolast.be>
2007-09-02 18:58:02 +0200

    Hackish implementation of per-pixel ideal path calculation

--- scarve.py
+++ scarve.py
@@ -66,15 +66,19 @@ class SeamCarve:
         def resize_width(self, pixels):
                 print "(%ds) Resizing width over %d pixels" % (int(time.mktime(time.localtime()) - self._init_time), pixels)
 
-                self._in_path = []
-                (w, h) = self._original.size
-                for y in range(0, h):
-                        self._in_path.append([False] * w)
+                self._resized = self._original.copy()
 
-                self._find_paths(pixels)
-                self._carve(pixels)
-                print "(%ds) Resizing done" % int(time.mktime(time.localtime()) - self._init_time)
-                self._flip_cost_image = False
+                for i in range(0, pixels):
+                        self._in_path = []
+                        (w, h) = self._resized.size
+                        for y in range(0, h):
+                                self._in_path.append([False] * w)
+
+                        x = self._find_best_path()
+                        self._find_path(x, True)
+                        self._carve(1)
+                        print "(%ds) Resizing %d done" % (int(time.mktime(time.localtime())) - self._init_time, i)
+                        self._flip_cost_image = False
 
         def resize_height(self, pixels):
                 #TODO This approach turns out to be very wrong. Needs to be rewritten nicely
@@ -107,8 +111,8 @@ class SeamCarve:
         def _calculate_energy(self):
                 print "(%ds) Calculating energy" % int(time.mktime(time.localtime()) - self._init_time)
 
-                (w, h) = self._original.size
-                gs = ImageOps.grayscale(self._original)
+                (w, h) = self._resized.size
+                gs = ImageOps.grayscale(self._resized)
                 im = numpy.reshape(gs.getdata(), (h, w))
 
                 r = generic_gradient_magnitude(im, derivative = sobel)
@@ -117,7 +121,7 @@ class SeamCarve:
                 self._energy.putdata(list(r.flat))
 
         def _calculate_costs(self):
-                (w, h) = self._original.size
+                (w, h) = self._resized.size
                 inf = float("infinity")
                 if self._energy == None:
                         self._calculate_energy()
@@ -154,7 +158,7 @@ class SeamCarve:
 
         def _find_path(self, basex, topdown):
                 #If basex is already in a path, search left and right for the closest pixel not in a path
-                (w, h) = self._original.size
+                (w, h) = self._resized.size
                 if topdown == True:
                         basey = 0
                 else:
@@ -230,23 +234,30 @@ class SeamCarve:
                 costs.sort()
                 return [t[1] for t in costs]
 
-        def _find_paths(self, n):
-                if self._costs == None:
-                        self._calculate_costs()
-
-                print "(%ds) Finding %d paths" % (int(time.mktime(time.localtime()) - self._init_time), n)
-
-                (w, h) = self._original.size
-                top_x_indices = self._get_x_indices_by_cost(0)
-                bottom_x_indices = self._get_x_indices_by_cost(h - 1)
-                top_last = False
-                for i in range(0, n):
-                        if top_last == False:
-                                self._find_path(top_x_indices[i], True)
-                                top_last = True
-                        else:
-                                self._find_path(bottom_x_indices[i], False)
-                                top_last = False
+        def _find_best_path(self):
+                bestx = 0
+                bestcost = float("infinity")
+                self._calculate_energy()
+                self._calculate_costs()
+                costs = self._costs
+                (w, h) = (len(costs[0]), len(costs))
+                for x in range(0, w):
+                        currcost = costs[0][x]
+                        currx = x
+                        for y in range(1, h):
+                                c = []
+                                if currx - 1 >= 0:
+                                        c.append((costs[y][currx - 1], currx - 1))
+                                c.append((costs[y][currx], currx))
+                                if currx + 1 < w:
+                                        c.append((costs[y][currx + 1], currx + 1))
+                                c.sort()
+                                currx = c[0][1]
+                                currcost = currcost + c[0][0]
+                        if currcost < bestcost:
+                                bestcost = currcost
+                                bestx = x
+                return x
 
         def get_path_image(self):
                 if self._energy == None:
@@ -289,14 +300,14 @@ class SeamCarve:
                 if self._in_path == None:
                         raise Exception, "No paths calculated"
 
-                (w, h) = self._original.size
+                (w, h) = self._resized.size
 
                 nw = w - dp
                 nh = h
 
-                ret = Image.new(self._original.mode, (nw, nh))
+                ret = Image.new(self._resized.mode, (nw, nh))
 
-                op = self._original.load()
+                op = self._resized.load()
                 rp = ret.load()
 
                 for y in range(0, h):
@@ -369,11 +380,11 @@ def main():
         carved.show()
         carved.save("carved.jpg")
         if verbose:
-                paths = c.get_paths_image()
-                paths.show()
-                c.get_energy_image().show()
-                c.get_costs_image().show()
-                c.get_paths_energy_image().show()
+#                paths = c.get_paths_image()
+#                paths.show()
+#                c.get_energy_image().show()
+#                c.get_costs_image().show()
+#                c.get_paths_energy_image().show()
                 image.show()
 
 if __name__ == "__main__":

a53f745d6b24dc45bf796b6f6ab51a91dfe6e5c3
Nicolas Trangez <ikke@nicolast.be>
2007-09-03 15:47:07 +0200

    Forgot license headers

--- sobel_energy_calculator.py
+++ sobel_energy_calculator.py
@@ -1,3 +1,26 @@
+# sobel_energy_calculator - Implementation of EnergyCalculator using
+#                           the Sobel matrix
+#
+# This file is part of "scarving", a Python implementation of the
+# seam carving image resize algorithm
+#
+# Copyright (C) 2007  Nicolas Trangez <eikke@eikke.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License (and no other).
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# EOL
+
 from energy_calculator import EnergyCalculator
 from scipy.ndimage.filters import generic_gradient_magnitude, sobel
 import sys
